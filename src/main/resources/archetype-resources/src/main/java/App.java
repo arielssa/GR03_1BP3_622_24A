9e5985318689b6b36c619e533c3d23d027b1f145
Ariel Sebastián Sánchez Añazco <121512401+arielssa@users.noreply.github.com>
2024-10-22 19:25:23 -0500

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ec.epn.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
